2028137a5f578d34ef406ea7852c60229cb1cf66
Gourav <gouravit1993@gmail.com>
2026-07-23 00:02:37 -0700

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -3,11 +3,26 @@ import { loadFragment } from '../fragment/fragment.js';
 
 export default async function decorate(block) {
   // load nav as fragment
+  console.log("test ");
   const navMeta = getMetadata('nav');
   const navPath = navMeta ? new URL(navMeta, window.location).pathname : '/nav';
   const fragment = await loadFragment(navPath);
-  console.debug(navMeta);
-   console.debug(navPath);
-   console.debug(fragment);
+  console.log(navMeta);
+   console.log(navPath);
+   console.log(fragment);
+
+   if (logo) {
+    // Replace the first section's contents with the authored image wrapped with a link to '/'
+    navBrand.innerHTML = `<a href="/" aria-label="Home" title="Home" class="home">${logo.outerHTML}</a>`;
+    // Make sure the logo is not lazy loaded as it's above the fold and can affect page load speed
+    navBrand.querySelector('img').settAttribute('loading', 'eager');
+  }
+
+  const navSections = nav.querySelector('.nav-sections');
+  if (navSections) {
+    // WKND: Remove Edge Delivery Services button containers and buttons from the nav sections links
+    navSections.querySelectorAll('.button-container, .button').forEach((button) => {
+      button.classList = '';
+    });
 
 }
